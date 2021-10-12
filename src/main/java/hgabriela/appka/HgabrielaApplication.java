d7f8262a6eb8fcccc434d7e1e1b1153534a8e745
Gabriela <gabrielabielsko@gmail.com>
2021-10-12 09:49:04 +0200

    commit

---
+++ src/main/java/hgabriela/appka/HgabrielaApplication.java
@@ -0,0 +1,13 @@
+package hgabriela.appka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgabrielaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HgabrielaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hgabriela/appka/HgabrielaApplicationTests.java
@@ -0,0 +1,13 @@
+package hgabriela.appka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HgabrielaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
